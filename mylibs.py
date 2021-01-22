aa1cac914d61ea55016fe3fb5385a622915263c5
marktongarcia <garcia.markanthony@gmail.com>
2021-01-22 14:02:26 +0100

    - fixed getpass for username

--- mylibs.py
+++ mylibs.py
@@ -1,13 +1,7 @@
+import paramiko
+import time
+from scp import SCPClient
 
-
-
-try:
-    import paramiko, getpass
-    import sys
-    import time
-    from scp import SCPClient
-except ImportError:
-    pass
 
 class Remote(object):
     """
@@ -20,20 +14,25 @@ class Remote(object):
         self.user = username
         self.password = password
         self.verbose = verbose
+        # if 'paramiko' in sys.modules.key():
+        #     self._has_paramiko = True
+        # else:
+        #     print('Please import paramiko module')
+        #     self._has_paramiko = False
 
     def __enter__(self):
-        if not self._has_paramiko():
-            raise ModuleNotFoundError('Please import paramiko')
-        else:
-            self.ssh_client = paramiko.SSHClient()
-            self.ssh_client.load_system_host_keys()
-            self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            print(f'\nConnecting to {self.host}...')
-            self.ssh_client.connect(hostname=self.host, username=self.user, password=self.password)
-            self.shellchannel = self.ssh_client.invoke_shell()
-            print('Successfully connected')
+        # if not self._has_paramiko():
+        #     raise ModuleNotFoundError('Please import paramiko')
+        # else:
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.load_system_host_keys()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # to avoid
+        # paramiko.ssh_exception.SSHException: Server '192.168.1.201' not found in known_hosts
+        print(f'\nConnecting to {self.host}...')
+        self.ssh_client.connect(hostname=self.host, username=self.user, password=self.password)
+        self.shellchannel = self.ssh_client.invoke_shell()  # this is opening a channel where you send and recv.
+        print('Successfully connected')
         return self
-
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         try:
@@ -45,12 +44,10 @@ class Remote(object):
         finally:
             self.ssh_client = None
 
-
-
     def execute(self, command, sudo=False):
-        if not self._has_paramiko():
-            print('self has no paramiko')
-            return None, None, None
+        # if not self._has_paramiko():
+        #     print('self has no paramiko')
+        #     return None, None, None
 
         stdin, stdout, stderr = self.execute(command)
         exit(1)
@@ -60,12 +57,12 @@ class Remote(object):
 
         return stdin, stdout, stderr
 
-    def get_shell(self):
-        # get shell
-        shellchannel = self.ssh_client.invoke_shell()
-        print('Successfully connected')
-        self.shell = shellchannel
-        return self
+    # def get_shell(self):
+    #     # get shell
+    #     shellchannel = self.ssh_client.invoke_shell()
+    #     print('Successfully connected')
+    #     self.shell = shellchannel
+    #     return self
 
     def shell(self, command, timeout=1):
         if self.verbose:
@@ -77,7 +74,7 @@ class Remote(object):
         # print(shellchannel.recv(10000).decode('utf-8'))
         # return self
 
-    def sendcmd(self, file_name, timeout=5):
+    def send_from_file(self, file_name):
         # print(self.shell)
         # exit(1)
         if self.verbose:
@@ -114,14 +111,16 @@ class Remote(object):
     def show(self, n=10000):
         # print(self.shell)
         # exit(1)
-        print(self.shellchannel.recv(n).decode('utf-8'))
+        # print(self.shellchannel.recv(n).decode('utf-8'))
+
+        return self.shellchannel.recv(n).decode('utf-8')
         # output = self.shell.recv(n)
         # return output.decode('utf-8')
         # print(output.decode())
 
+    # def _has_paramiko(self):
+    #     return 'paramiko' in sys.modules.keys()
 
-    def _has_paramiko(self):
-        return 'paramiko' in sys.modules.keys()
 
 # def get_auth():
 #     print("Enter your ssh credentials")
@@ -135,15 +134,22 @@ class Remote(object):
 #     get_auth()
 
 if __name__ == '__main__':
-    with Remote(hostname='192.168.1.201', username='cisco', password='cisco', verbose=True) as x:
-    # with Remote(hostname='192.168.1.12', username='gns3', password='gns3', verbose=True) as x:
-        print(x.__init__)
-        # exit(1)
-        ## sample shell()
-        # x.shell('terminal length 0\n')
-        ## sample sendcmd()
-        x.sendcmd('./commands.txt')
-        if x.verbose:
-            x.show()
-        ## sample scp_conhnect()
-        # x.scp_connect('deleteme.txt', 'tae.txt', command='download')
+    devices = ['192.168.1.201', '192.168.1.202']
+    for dev in devices:
+        with Remote(hostname=dev, username='cisco', password='cisco', verbose=True) as x:
+            '''
+            # with Remote(hostname='192.168.1.12', username='gns3', password='gns3', verbose=True) as x:
+            print(x.__init__)
+            exit(1)
+            # sample shell()
+            x.shell('terminal length 0\n')
+            '''
+            # sample sendcmd()
+            x.send_from_file('./commands.txt', verbose=False)
+            if x.verbose:
+                output = x.show()
+                print(output)
+            '''
+            # sample scp_connect()
+            x.scp_connect('deleteme.txt', 'tae.txt', command='download')
+            '''
